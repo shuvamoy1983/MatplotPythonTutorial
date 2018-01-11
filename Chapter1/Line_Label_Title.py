ad279b6e0ba7a89f0111f920e1a105a404234250
shuvamoy <shuvamoy1983@gmail.com>
2018-01-11 01:01:17 -0600

    Changes for file naming

--- Chapter1/Line_Label_Title.py
+++ Chapter1/Line_Label_Title.py
@@ -1,5 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([1,2,3],[6,7,4])
+x = []
+y = []
+
+x1 = []
+y1 = []
+for i in range(4):
+    x.append(i)
+    y.append(np.sin(i))
+    x1.append(i)
+    y1.append(np.sin(i)/2)
+
+###label is used to identify the graph details within plot
+plt.plot(x,y,label="First_Line")
+plt.plot(x1,y1,label="Second_Line")
+plt.xlabel('X-Axis_Val')
+plt.ylabel('Y-Axis_Val')
+plt.title("Simple Graph\nCheck It Out")
+###legend is used to print all the label inside the graph
+plt.legend()
 plt.show()
